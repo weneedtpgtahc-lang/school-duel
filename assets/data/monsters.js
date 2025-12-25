cb8b9e4347235bbb57950c8fd9e1465c680d88bf
weneedtpgtahc-lang <weneedtpgtahc@gmail.com>
2025-12-25 20:31:24 +0900

    Create monsters.js

---
+++ assets/data/monsters.js
@@ -0,0 +1,91 @@
+// assets/data/monsters.js
+// 바이러스 몬스터 데이터 (이미지 확정, 스탯/효과 미확정)
+
+export const virusMonsters = [
+  // ===== 하급 =====
+  {
+    id: "virus_low_1",
+    name: "침투 포자",
+    tier: "low",
+    image: "assets/virus/virus_low_1.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+  {
+    id: "virus_low_2",
+    name: "복제 세포",
+    tier: "low",
+    image: "assets/virus/virus_low_2.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+  {
+    id: "virus_low_3",
+    name: "점액 분열체",
+    tier: "low",
+    image: "assets/virus/virus_low_3.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+
+  // ===== 중급 =====
+  {
+    id: "virus_mid_1",
+    name: "감염 확산체",
+    tier: "mid",
+    image: "assets/virus/virus_mid_1.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+  {
+    id: "virus_mid_2",
+    name: "변이 돌격체",
+    tier: "mid",
+    image: "assets/virus/virus_mid_2.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+  {
+    id: "virus_mid_3",
+    name: "은폐 잠복체",
+    tier: "mid",
+    image: "assets/virus/virus_mid_3.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+
+  // ===== 상급 =====
+  {
+    id: "virus_high_1",
+    name: "숙주 지배체",
+    tier: "high",
+    image: "assets/virus/virus_high_1.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+  {
+    id: "virus_high_2",
+    name: "신경 오염체",
+    tier: "high",
+    image: "assets/virus/virus_high_2.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  },
+  {
+    id: "virus_high_3",
+    name: "재앙 군체핵",
+    tier: "high",
+    image: "assets/virus/virus_high_3.png",
+    atk: null,
+    hp: null,
+    effect: ""
+  }
+];
